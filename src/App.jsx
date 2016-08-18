7bbe6791f8546c9eb1840f6c674c87f1510e1b93
Suhair <suhairzain11@gmail.com>
2016-08-18 19:55:46 +0530

    Added option for proxy and file name

--- src/App.jsx
+++ src/App.jsx
@@ -23,14 +23,33 @@ class App extends React.Component {
         },
         title: {
             color: '#FFFFFF',
-            fontSize: '3em'
+            fontSize: '3em',
+            marginBottom: 24
+        },
+        fields: {
+            display: 'flex',
+            flexDirection: 'column',
+            width: '60%'
         },
         inputAndButton: {
             display: 'flex',
             width: '100%'
         },
-        input: {
-            flexGrow: 1
+        nameAndProxy: {
+            display: 'flex',
+            margin: '8px 0',
+            width: '100%'
+        },
+        nameInput: {
+            marginRight: 4,
+            width: '50%'
+        },
+        proxy: {
+            alignItems: 'center',
+            backgroundColor: '#fff',
+            display: 'flex',
+            marginLeft: 4,
+            width: '50%'
         },
         footer: {
             backgroundColor: '#d32f2f',
@@ -42,20 +61,72 @@ class App extends React.Component {
         }
     };
 
-    renderButton = (url) => {
-        gapi.savetodrive.render('save-drive-button', {
-            src: 'https://sz-node-proxy.herokuapp.com/' + url,
-            filename: 'My Statement.pdf',
-            sitename: 'My Company Name'
-        });
+    constructor(props){
+        super(props);
+
+        const url = 'http://s3.amazonaws.com/akamai.netstorage/HD_downloads/earth_night_rotate_1080.mov';
+
+        this.state = {
+            url: url,
+            name: this.getNameFromUrl(url),
+            proxy: false
+        }
+    }
+
+    validUrl = (url) => {
+        return !(new RegExp('/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/').test(url))
+    };
+
+    renderButton = (proxy) => {
+        const {url, name} = this.state;
+        if(proxy===undefined)
+            proxy = this.state.proxy;
+
+        if(this.validUrl(url)) {
+            gapi.savetodrive.render('save-drive-button', {
+                src: proxy?'https://sz-node-proxy.herokuapp.com/' + url: url,
+                filename: name,
+                sitename: 'SZ - URL to GDrive'
+            });
+        } else {
+            alert('Invalid URL. Please enter a valid URL.')
+        }
     };
 
     componentDidMount = () => {
-        this.renderButton('http://s3.amazonaws.com/akamai.netstorage/HD_downloads/earth_night_rotate_1080.mov');
+        this.renderButton();
+    };
+
+    getNameFromUrl = (url) => {
+        return url.substr(url.lastIndexOf('/') + 1).replace(new RegExp('_', 'g'), ' ')
     };
 
     handleUrlChange = (evt) => {
-        this.renderButton(evt.target.value);
+        const url = evt.target.value;
+
+        this.setState({
+            ...this.state,
+            url: url,
+            name: this.validUrl(url)?this.getNameFromUrl(url):'INVALID URL'
+        });
+        this.renderButton(url)
+    };
+
+    onFileNameChange = (evt) => {
+        this.setState({
+            ...this.state,
+            name: evt.target.value
+        });
+        this.renderButton()
+    };
+
+    onProxyChange = () => {
+        const proxy = this.state.proxy;
+        this.setState({
+            ...this.state,
+            proxy: !proxy
+        });
+        this.renderButton(!proxy)
     };
 
     render() {
@@ -63,10 +134,22 @@ class App extends React.Component {
             <div style={this.styles.root}>
                 <div style={this.styles.content}>
                     <span style={this.styles.title}>Upload from a Url to your Google Drive</span>
-                    <div style={this.styles.inputAndButton}>
-                        <input onChange={this.handleUrlChange} style={this.styles.input}/>
-                        <div id="save-drive-button"></div>
+                    <div style={this.styles.fields}>
+                        <input value={this.state.url} onChange={this.handleUrlChange}/>
+                        <div style={this.styles.nameAndProxy}>
+                            <input
+                                value={this.state.name}
+                                onChange={this.onFileNameChange} style={this.styles.nameInput}/>
+                            <div style={this.styles.proxy}>
+                                <input
+                                    type="checkbox"
+                                    onChange={this.onProxyChange}
+                                    checked={this.state.proxy}/>
+                                <span>Use proxy. Use this option on fail.</span>
+                            </div>
+                        </div>
                     </div>
+                    <div id="save-drive-button"></div>
                 </div>
                 <div style={this.styles.footer}>
                     <a style={this.styles.footerLink} href="https://github.com/SuhairZain">Source</a>
